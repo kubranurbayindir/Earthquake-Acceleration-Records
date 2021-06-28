79ee54dee5303d2ae99f5d8a041a483045035575
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2021-06-28 20:56:52 +0300

    Update Sidebar.jsx

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -8,7 +8,10 @@ export default function Sidebar() {
     }
 
     return (
-        <div>
+        <div  style={{
+            width: "100%",
+            height: "100%",
+          }}>
             <Link style={navStyle} to='/bodxchart'>    <Segment color='red'>TDG Bod X Record Chart</Segment> </Link> <br></br>
             <Link style={navStyle} to='/bodychart'> <Segment color='green'>TDG Bod Y Record Chart</Segment> </Link><br></br>
             <Link style={navStyle} to='/bodzchart'> <Segment color='blue'>TDG Bod Z Record Chart</Segment> </Link>
