397c549190dfb3e60ff6554fdcc064508ef830ed
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2021-06-28 21:15:55 +0300

    Add files via upload

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,7 +3,6 @@ import Sidebar from "./Sidebar.jsx";
 import Bodxchart from "../components/Bodxchart.js";
 import Bodychart from "../components/Bodychart.js";
 import Bodzchart from "../components/Bodzchart.js";
-import { Row, Col } from 'antd';
 import { Grid, Container, Header } from "semantic-ui-react";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -15,10 +14,10 @@ function Dashboard() {
       <div>
         <Grid>
           <Grid.Row>
-            <Grid.Column width={4} gutter={[16, 16]}>
+            <Grid.Column width={4}>
               <Sidebar />
             </Grid.Column>
-            <Grid.Column width={12} >
+            <Grid.Column width={12}>
               <Route path="/" component={Home} />
               <Route path="/bodxchart" component={Bodxchart} />
               <Route path="/bodychart" component={Bodychart} />
