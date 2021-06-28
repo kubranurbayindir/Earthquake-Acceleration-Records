d3faf11d13d168ab055033ee419fbed1be5f8e58
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2021-06-28 20:58:01 +0300

    Update Dashboard.jsx

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -11,7 +11,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function Dashboard() {
   return (
     <Router>
-      <div>
+      <div  style={{
+            width: "100%",
+            height: "100%",
+          }}>
         <Grid>
           <Grid.Row>
             <Grid.Column width={4}>
@@ -31,7 +34,10 @@ function Dashboard() {
 }
 
 const Home = () => (
-  <div>
+  <div  style={{
+            width: "100%",
+            height: "100%",
+          }}>
     <Container text style={{ marginBottom: '2em' }}>
       <Header as='h1'>TDG Earthquake Acceleration Records </Header>
       <Header as='h3'>✨ It is always ready to be developed and renewed! ✨ </Header>
